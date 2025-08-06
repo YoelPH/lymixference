7ef51d07599a36c47ce713cdc5cf27c9eae4ab8f
YoelPH <yoelhaas@gmail.com>
2025-08-06 08:54:16 +0200

    filtering changes

--- scripts/filter_and_reduce.py
+++ scripts/filter_and_reduce.py
@@ -110,9 +110,9 @@ for icd_list in OROPHARYNX_ICD_CODES.values():
         icd_codes.append(icd)
 
 # Add ICD codes from HYPOPHARYNX_ICD_CODES
-for icd_list in HYPOPHARYNX_ICD_CODES.values():
-    for icd in icd_list:
-        icd_codes.append(icd)
+# for icd_list in HYPOPHARYNX_ICD_CODES.values():
+#     for icd in icd_list:
+#         icd_codes.append(icd)
 
 # Add ICD codes from LARYNX_ICD_CODES
 # for icd_list in LARYNX_ICD_CODES.values():
